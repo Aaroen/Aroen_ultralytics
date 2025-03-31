e94e186e4325afa141d389b40d9ee0095599f661
Aaroen <1126904957@qq.com>
2025-03-31 20:50:34 +0800

    修正YOLO模型训练示例中的数据集路径，将反斜杠改为正斜杠，以确保路径在不同操作系统上的兼容性。

--- Z_2_Training_Example.py
+++ Z_2_Training_Example.py
@@ -3,12 +3,11 @@ import multiprocessing
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
-    
     # 加载模型
     model = YOLO("ultralytics/cfg/models/12/yolo12n.yaml").load("yolo12n.pt")  
     # 训练模型
     results = model.train(
-        data="ultralytics\cfg\datasets\VOC.yaml",
+        data="ultralytics/cfg/datasets/VOC.yaml",
         epochs=3,
         imgsz=640,          
         device=0,
